a4991b2fd67c9e3894c35215f659965d93319326
Oliver Maiyo <olivermaiyo@koech-2.local>
2022-10-25 00:24:04 +0300

    style: styling hero section

---
+++ src/Components/Hero.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./style.css";
+
+
+const Hero = () => {
+  return (
+    <section>
+      <div className="hero-section">
+        <div className="image-container">
+          <img
+            src="https://raw.githubusercontent.com/Olliemint/Airbnb-experiences/main/src/images/Group%2077.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero
