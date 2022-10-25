b0458a2f5d8c19c23d4c9b1366a88c9ffdfb6d45
Oliver Maiyo <olivermaiyo@koech-2.local>
2022-10-25 16:59:05 +0300

    style: styling hero section

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -6,11 +6,20 @@ const Hero = () => {
   return (
     <section>
       <div className="hero-section">
-        <div className="image-container">
+        <div className="image-contain">
           <img
-            src="https://raw.githubusercontent.com/Olliemint/Airbnb-experiences/main/src/images/Group%2077.png"
-            alt=""
+            className="hero-image"
+            src="https://raw.githubusercontent.com/Olliemint/Airbnb-experiences/main/src/images/Group77.png"
+            alt="no image"
           />
+        </div>
+        <div className="hero-details">
+          <h1 className="hero-title">Online Experiences</h1>
+          <p className="h-message">
+            Join unique interactive activities led by
+            <br />
+            one-of-a-kind hosts—all without leaving home
+          </p>
         </div>
       </div>
     </section>
